7c355962181540296da4309d81850dc68eb804ec
Donald KANTI <donaldkant2000@gmail.com>
2023-09-16 22:33:20 +0100

    updates

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,7 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
-import { AppService, ToolDto } from './app.service';
+import { Controller } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('tools/:name')
-  getToolByName(@Param('name') name: string): string {
-    return this.appService.findToolByName(name);
-  }
-
-  @Get('tools')
-  getTools(): string[] {
-    return this.appService.findTools()
-  }
-
-  @Post('tools')
-  createTool(@Body() body: ToolDto): string[] {
-    return this.appService.createTool(body);
-  }
-
-
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,39 +4,6 @@ import { IsNotEmpty, IsString } from 'class-validator';
 @Injectable()
 export class AppService {
 
-  constructor() {
+  constructor() {}
 
-  }
-
-  tools: string[] = [
-    "knife",
-    "drill",
-    "scissors",
-    "hammer"
-  ]
-
-  findTools() {
-    return this.tools;
-  }
-
-  findToolByName(name: string) {
-    let tool = this.tools.find(tool => tool === name);
-    if (!tool) {
-      throw new NotFoundException(`Tool of that name does not exist`)
-    }
-    return tool;
-  }
-
-  createTool(toolPayload: ToolDto) {
-    this.tools.push(toolPayload.name);
-    return this.tools;
-  }
-
-
-}
-
-export class ToolDto {
-
-  @IsString()
-  name: string;
 }
