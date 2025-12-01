ee7a7ec6fc8f8430ccba91742d06551ec92d1a3a
Donald KANTI <donald.kanti@mehriso.com>
2025-12-01 15:52:12 +0100

    update code

--- src/whatsapp/dtos/request.dto.ts
+++ src/whatsapp/dtos/request.dto.ts
@@ -4,9 +4,9 @@ import { IsPhoneNumberWithCountryCode } from "./phone-number-validator";
 
 export class WhatsappMessagePayloadDto {
 
-    // must include country code, e.g., 2290162798845
+    // must include country code, eg: +2290162798845
     @IsPhoneNumberWithCountryCode({
-        message: 'Veuillez fournir un numéro de téléphone valide avec le code pays (ex: +229 XX XX XX XX)',
+        message: 'Veuillez fournir un numéro de téléphone valide avec le code pays (ex: +2290162798845 / +229 01 62 79 88 45)',
     })
     phoneNumber: string;
 
