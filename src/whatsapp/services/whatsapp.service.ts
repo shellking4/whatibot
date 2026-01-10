1ad0b9d7b263ea5b1e13601fd3471d6180b72d07
Donald KANTI <donald.kanti@mehriso.com>
2026-01-10 14:45:44 +0100

    update

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -66,6 +66,7 @@ export class WhatsappService {
                     '--disable-accelerated-2d-canvas',
                     '--no-first-run',
                     '--no-zygote',
+                    '--single-process',
                     '--disable-gpu'
                 ],
             },
