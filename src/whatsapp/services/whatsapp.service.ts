c9600a8d697d740b4e8f3b36277732cac0eae8cf
Donald KANTI <donald.kanti@mehriso.com>
2026-01-19 13:42:10 +0100

    update code

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -58,7 +58,7 @@ export class WhatsappService {
         
         const client = new Client({
             puppeteer: {
-                headless: false,
+                headless: true,
                 args: [
                     '--no-sandbox',
                     '--disable-setuid-sandbox',
